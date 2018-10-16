16f7b83e68e32af9932a779223e5437f956e8afc
liqianqian <liqq@quantdo.com.cn>
2018-10-16 13:21:33 +0800

    添加预警视图model

---
+++ src/models/alerts.js
@@ -0,0 +1,27 @@
+export default {
+    namespace: 'alerts',
+
+    state: {
+        echartsArray:[]
+    },
+
+    effects: {
+
+    },
+
+    reducers: {
+        save(state, { payload }) {
+            return {
+                ...state,
+                ...payload
+            };
+        },
+        size(state, { payload }) {
+            let item = state.echartsArray.find((ele) => ele.key == payload);
+            if (!!item) {
+                item.echart.resize()
+            }
+            return state;
+        },
+    },
+};
