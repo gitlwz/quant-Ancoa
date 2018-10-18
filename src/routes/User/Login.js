fbeb14d23e8d940bd0f27b3415631da0883ae502
liuwenzhu <547485261@qq.com>
2018-10-18 13:52:29 +0800

    add
    Signed-off-by: liuwenzhu <547485261@qq.com>

--- src/routes/User/Login.js
+++ src/routes/User/Login.js
@@ -45,10 +45,6 @@ class LoginPage extends Component {
             <div className={'qdp-login-main'}>
                 <Login defaultActiveKey={type} onTabChange={this.onTabChange} onSubmit={this.handleSubmit}>
                     <Tab key="account" tab="账户密码登录">
-                        {login.status === 'error' &&
-                            login.type === 'account' &&
-                            !submitting &&
-                            this.renderMessage('账户或密码错误（admin/888888）')}
                         <UserName name="userName" placeholder="admin/user" />
                         <Password name="password" placeholder="888888/123456" />
                     </Tab>
